3866898e08ec991966d5dc29fb7a83a94e63c465
Jaden Schuster <penguins8mylunch@gmail.com>
2024-09-07 01:12:23 -0400

    Changed port to not interfer with other bots

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   res.send("hello world!")
 })
 
-app.listen(3000, () => { console.log("YappingBot Ready!") });
+app.listen(3001, () => { console.log("YappingBot Ready!") });
 
 function load() {
   try {
